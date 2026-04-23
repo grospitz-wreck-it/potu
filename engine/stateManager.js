3b2fea9299e6fcf342b9b19487d2db2cca1b61b6
grospitz-wreck-it <grospitz@gmail.com>
2026-04-23 22:24:49 +0200

    Update stateManager.js

--- engine/stateManager.js
+++ engine/stateManager.js
@@ -1,1 +1,10 @@
-a
+import { supabase } from "../api/supabaseClient"
+
+export const saveGameState = async (state) => {
+  await supabase
+    .from("game_state")
+    .update({
+      turn: state.turn + 1
+    })
+    .eq("id", 1)
+}
