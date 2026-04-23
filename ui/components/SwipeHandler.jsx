0c2b1eebe46f562e75b2062579e87a30da0c60b5
grospitz-wreck-it <grospitz@gmail.com>
2026-04-23 22:36:44 +0200

    Update SwipeHandler.jsx

--- ui/components/SwipeHandler.jsx
+++ ui/components/SwipeHandler.jsx
@@ -1,1 +1,28 @@
-a
+import { useRef } from "react"
+
+export const SwipeHandler = ({ onSwipe, children }) => {
+  const startX = useRef(0)
+
+  const handleStart = (e) => {
+    startX.current = e.touches ? e.touches[0].clientX : e.clientX
+  }
+
+  const handleEnd = (e) => {
+    const endX = e.changedTouches ? e.changedTouches[0].clientX : e.clientX
+    const diff = endX - startX.current
+
+    if (diff > 50) onSwipe("right")
+    if (diff < -50) onSwipe("left")
+  }
+
+  return (
+    <div
+      onMouseDown={handleStart}
+      onMouseUp={handleEnd}
+      onTouchStart={handleStart}
+      onTouchEnd={handleEnd}
+    >
+      {children}
+    </div>
+  )
+}
